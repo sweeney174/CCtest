214f54e6b6fe5a1b6910d3d868a8852f455fe1d6
sweeney174 <1749790593@qq.com>
2020-10-18 20:35:26 +0800

    第一次电控组作业

---
+++ task1/q12.c
@@ -0,0 +1,22 @@
+//电控组第一次作业 
+//物理202 覃天凤
+//第一题
+
+include<stdio.h>//开头缺少#
+main//应为int main（）{}
+(  
+    float g;h;//应为float g,h;
+    float tax,rate;//rate未赋值
+    g=e21;//科学计数法应写为g=1e21
+    tax=rate*g;
+    //无返回值
+)
+
+/*
+    第二题
+    a.string
+    b.int
+    c.float
+    d.int(16进制)
+    e.float(科学计数法)
+*/
---
+++ task1/q3.c
@@ -0,0 +1,9 @@
+//电控组第一次作业
+//第三题
+//物理202 覃天凤
+
+#include <stdio.h>
+int main(){
+    printf("Startled by the sudden sound,Sally shouted,\n\"By the Great Pumpkin,What was that!\"");
+    return 0;
+}
